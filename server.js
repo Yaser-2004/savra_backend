fcad15fcdd133cc1f715df7d195b2858aceb6f9b
Yaser Siddiquee <yaser.siddiquee@gmail.com>
2026-05-16 21:48:58 +0530

    second commit

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ["https://savra-delta.vercel.app", "http://localhost:5173"],
+    credentials: true
+}));
 app.use(express.json());
 
 // serve generated PPT files
